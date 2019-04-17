20e11fc9e1eb1c0235e24551a84b919aded9c0ba
Ilija Mihajlovic <ikacdrake619@gmail.com>
2019-04-17 02:17:21 -0700

    update

--- CoreMLAndVisionObjectClassifierLightweightVersion/CoreMLAndVisionObjectClassifierLightweightVersion/Controller/MainVC.swift
+++ CoreMLAndVisionObjectClassifierLightweightVersion/CoreMLAndVisionObjectClassifierLightweightVersion/Controller/MainVC.swift
@@ -80,6 +80,9 @@ class MainVC: UIViewController{
         }
     }()
     
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
